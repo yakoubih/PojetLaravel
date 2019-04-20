c295a707eb570a55f5c52c202b1aac2f1b5feb5c
yakoubih <yhocine.yakoubi@gmail.com>
2019-04-20 11:01:36 +0200

    Modif sur message de cofirmation2

--- resources/views/contact/confirm.blade.php
+++ resources/views/contact/confirm.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts/main')
 @section('content')
 <script type="text/javascript">
-	alert("Etes-vous sure de votre message?")
+	document.write("Etes-vous sure de votre message?")
 </script>
-<h1>Nous avons bien reçu votre requête et allons vous répondre dans les plus bref delais<br>Merci à bientot !  </h1>
+<h1>Nous avons bien reçu votre demande et allons vous répondre dans les plus bref delais<br>Merci à bientot !  </h1>
 
 
 @endsection

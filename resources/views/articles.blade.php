8efc0e2b8d89488f25ad41428c46072cc05db1a3
geekhamza <derdjini.hamza@gmail.com>
2019-04-20 22:58:46 +0200

    Add_Comment_Form

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -15,6 +15,23 @@
 <ul class="menu simple">
 <li><a  href="articles/{{$post->post_name}}" style="color: white">{{ $post->post_title }}</a></li>
 <li><a href="#" style="color: white">Comments: 3</a></li>
+
+                    <form action="{{ url('/comment') }}" method="POST" novalidate>
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <input type="text" class="form-control {{ $errors->has('comment_name') ? 'is-invalid' : '' }}" name="comment_name" id="comment_name" placeholder="Votre nom"
+                                value="{{ old('comment_name') }}" required="required"> {!! $errors->first('comment_name', '<div class="invalid-feedback">:message</div>') !!}
+                        </div>
+                        <div class="form-group" >
+                            <input type="email" class="form-control {{ $errors->has('comment_email') ? 'is-invalid' : '' }}" name="comment_email" id="comment_email" placeholder="Votre email"
+                                value="{{ old('comment_email') }}"> {!! $errors->first('comment_email', '<div class="invalid-feedback">:message</div>') !!}
+                        </div>
+                        <div class="form-group ">
+                            <textarea class="form-control {{ $errors->has('comment_content') ? 'is-invalid' : '' }}" name="comment_content" id="comment_content" placeholder="Votre commentaire">{{ old('comment_content') }}</textarea>                            {!! $errors->first('comment_content', '<div class="invalid-feedback has-error">:message</div>') !!}
+                        </div>
+                        <button type="submit" class="btn btn-secondary" style="color: white">Commenter</button>
+                    </form>
+
 </ul>
 </div>
 </div>

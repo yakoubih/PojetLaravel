b31155d69880d91c72fda6961e194b056af71dec
yakoubih <yhocine.yakoubi@gmail.com>
2019-04-21 14:58:58 +0200

    image

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -9,8 +9,7 @@
   <li> {{ $post->post_title }} </li>
 <div class="blog-post">
 <!--<h3>Awesome blog post title <small>3/6/2015</small></h3>-->
-<img class="thumbnail" src="https://placehold.it/850x350">
-
+<img class="thumbnail" src="photos/image{{$post->id}}.jpg">
 <div class="list-group-item container jumbotron" style = "width: 860px; margin-bottom: 42px;background-color: gray;">
 <ul class="menu simple">
 <li><a  href="articles/{{$post->post_name}}" style="color: white">{{ $post->post_title }}</a></li>
--- resources/views/contact/confirm.blade.php
+++ resources/views/contact/confirm.blade.php
@@ -3,7 +3,7 @@
 <script type="text/javascript">
 	alert("Etes-vous sure de votre message?")
 </script>
-<h1>Nous avons bien reçu votre demande et allons vous répondre dans les plus bref delais<br>Merci à bientot !  </h1>
+<h1 style="text-align: center">Nous avons bien reçu votre demande et allons vous répondre dans les plus bref delais<br>Merci à bientot !  </h1>
 
 
 @endsection
